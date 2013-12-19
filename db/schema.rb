f32ebe9c4ffb7b6914829253436ffbac3b98b0e2
Joe Falls <joe@Joes-MacBook-Air.local>
2013-12-18 22:50:58 -0800

    next changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219022014) do
+ActiveRecord::Schema.define(version: 20131219061001) do
 
   create_table "statuses", force: true do |t|
     t.string   "name"
